d2ef3908779f93bdc2077134c6d39e1562b47206
Himfile <klinker17@mail.ru>
2020-04-22 14:41:34 +0300

    Added two arrays with initial  data

--- Tirage/Program.cs
+++ Tirage/Program.cs
@@ -6,6 +6,9 @@ namespace Tirage {
         static void Main (string[] args) {
             int b = 0;
             int x = 0;
+            int[] Sides = {20, 24 ,36, 45, 49};
+            int[] xBool = { 4, 5, 6, 7, 8, 12};
+
             Random dice = new Random ();
             for (int i = 0; i < 5; i++) {
                 try {
@@ -14,8 +17,8 @@ namespace Tirage {
                     Console.WriteLine ("Введите число 4-8,12");
                     continue;
                 }
-                if (x != 4 && x != 5 && x != 6 && x != 7 && x != 8 && x != 12) {
-                    Console.WriteLine ("Введите число 4-7,12");
+                if(Array.IndexOf(xBool, x) < 0) { 
+                    Console.WriteLine ("Введите число 4-8,12");
                     continue;
                 } else break;
             }
@@ -31,12 +34,12 @@ namespace Tirage {
             Console.ReadKey ();
             int GetSide (int a) {
                 switch (a) {
-                    case 4: b = 20; break;
-                    case 5: b = 36; break;
-                    case 6: b = 45; break;
-                    case 7: b = 49; break;
-                    case 8: b = 20; break;
-                    case 12: b = 24; break;
+                    case 4: b = Sides[0]; break;
+                    case 5: b = Sides[2]; break;
+                    case 6: b = Sides[3]; break;
+                    case 7: b = Sides[4]; break;
+                    case 8: b = Sides[0]; break;
+                    case 12: b = Sides[1]; break;
                 }
                 return a;
             }
